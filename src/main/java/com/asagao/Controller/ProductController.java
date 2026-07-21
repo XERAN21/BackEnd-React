2e9ae9d9708d38ab18997be1a28ba3ed75e6042a
ichinose <ichinose@email.com>
2026-07-21 15:18:52 +0900

    修正

--- src/main/java/com/asagao/Controller/ProductController.java
+++ src/main/java/com/asagao/Controller/ProductController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import jakarta.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class ProductController {
 
 	private final ProductService productService;
